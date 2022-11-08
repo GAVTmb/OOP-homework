435258d5cf703caba8668c8514ded7dbadf4100f
Alexandr Galayura <first6886@mail.ru>
2022-11-08 19:32:43 +0300

    add homework

---
+++ Открытие и чтение файла, запись в файл/Open_and_read  file_Connecting files.py
@@ -0,0 +1,27 @@
+dict_file = {1: '1.txt', 2: '2.txt', 3: '3.txt'}
+
+def reading_files(dict):
+    general_vocabulary = {}
+    for key, values in dict.items():
+        with open(values, encoding='utf-8') as file:
+            name_file = values
+            list_of_strings = [values]
+            number_of_rows = 0
+            for line in file:
+                list_of_strings.append(line)
+                number_of_rows += 1
+            general_vocabulary[number_of_rows] = list_of_strings
+    return general_vocabulary
+
+
+def file_recording():
+    text_to_write = reading_files(dict_file)
+    sort_text = dict(sorted(text_to_write.items()))
+    for key, values in sort_text.items():
+        with open('result.txt', 'at', encoding='utf-8') as resault:
+            resault.write(f'{values[0]}\n')
+            resault.write(f'{str(key)}\n')
+            resault.write(f'{" ".join(values[1:])}\n')
+
+
+file_recording()
